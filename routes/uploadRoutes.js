e7472679c5f886fe498347ad38e45ed3e2c66f4a
Ritik Pal <ritikdh34@gmail.com>
2026-09-22 23:22:31 -0700

    Fix Cloudinary image upload

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -19,7 +19,7 @@ router.post("/", upload.single("document"), async (req, res) => {
 
     // Upload file to Cloudinary
     const uploadResult = await cloudinary.uploader.upload(req.file.path, {
-      resource_type: "image",
+      resource_type: "auto",
       folder: "ricozdocai-documents",
     })
 
